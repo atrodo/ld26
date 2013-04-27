69ed54c948263ddab76670ed0fb8daaf409602fb
Jon Gentle <atrodo@atrodo.org>
2013-04-27 05:19:51 -0400

    Template for ld26

---
+++ ld26.js
@@ -0,0 +1,7 @@
+[% INCLUDE "$game_js/flower.js" %]
+[% INCLUDE "$game_js/puzzle.js" %]
+[% INCLUDE "$game_js/puzzle_flower.js" %]
+[% INCLUDE "$game_js/puzzle_board.js" %]
+[%# INCLUDE "$game_js/game_setup.js" %]
+
+[% INCLUDE "$game_js/game_tiles.js" %]
