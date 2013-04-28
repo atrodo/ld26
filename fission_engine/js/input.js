b2d6a771da50b92ae9fcdfb2acf1e8b552659a2d
Jon Gentle <atrodo@atrodo.org>
2013-04-28 10:09:59 -0400

    It's enter, not return

--- fission_engine/js/input.js
+++ fission_engine/js/input.js
@@ -11,7 +11,7 @@
     var active_actions = {}
 
     var special_keys = {
-      8:  "backspace", 9:  "tab",   13: "return", 16: "shift",
+      8:  "backspace", 9:  "tab",   13: "enter",  16: "shift",
       17: "ctrl",      18: "alt",   19: "pause",  20: "capslock",
       27: "esc",       32: "space", 33: "pageup", 34: "pagedown",
       35: "end",       36: "home",  37: "left",   38: "up",
